581c11a6143ae7d757b1b6e24c7e56e7e773f9fc
Ashish Bhardwaj <erashish41@gmail.com>
2024-11-26 15:13:01 +0530

    object initialised

---
+++ day7-Object/object.js
@@ -0,0 +1,47 @@
+// Object
+// Objects are containers for Properties and Methods.
+// Properties are named Values.
+// Methods are Functions stored as Properties
+// Properties can be primitive values, functions, or even other objects.
+
+
+// Activity 1: Object Creation and Access
+// Task 1: create a object represents a book with properties like title, author, and year, and log the object to the console.
+const book = {
+    title: "best in the world",
+    author: "william",
+    year: 2024,
+}
+console.log(book);
+
+// Task 2: access title and author in console.
+const title1 = book.title
+console.log(title1);
+
+const author1 = book.author
+console.log(author1);
+
+
+// Activity 2: Object Methods
+// : Task 3: Add a method to the book object that returns a string with the book's 
+//      title and author, and log the result of calling the method.
+
+
+// : Task 4: Add a method to the book object that takes a parameter(year) and updates
+//       the book's year property then log the updated object.
+
+// Activity 3: Nested Objects
+// : Task 5: Ccess and get harrep rene tirary and the titles oparties iens in the libraks (an array of book objects), and log the library object to the console.
+// Activity 4: The this Keyword
+// • Task 7: Add a method to the book object that uses the this keyword to return a string with the book's title and year, and log the result of calling this method.
+// Activity 5: Object Iteration
+// • Task 9: Use a fier. keyS and iterate aert merperties of the bo kelsent and les of che open and its value.
+// Feature Request:
+// 2: Lrary Object Siri : Create a scrpe that dernes a tay Objet containing an aray of bot objets and loes the dirarys detalts.
+// 3. Object Iteration Script: Write a script that demonstrates iterating over an object's properties using for... in loop and Object. keys / Object. values .
+// Achievement:
+// By the end of these activities, students will:
+// • Create and manipulate objects with properties and methods.
+// • Understand and use the this keyword in object methods.
+// • Work with nested objects and arrays of objects.
+// • Iterate over an object's properties using loops and built-in methods.
