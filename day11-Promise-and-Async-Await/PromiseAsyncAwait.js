6eb7abd1e4c942ba15fca3bafb5375b8bb3935af
Ashish Bhardwaj <erashish41@gmail.com>
2024-12-04 23:56:39 +0530

    updates

--- day11-Promise-and-Async-Await/PromiseAsyncAwait.js
+++ day11-Promise-and-Async-Await/PromiseAsyncAwait.js
@@ -146,7 +146,6 @@ async function ashishBhardwaj(){
 }
 
 
-
 // Activity 5: Concurrent Promises
 // • Task 8: Use Promise.all to wait for multiple promises to resolve and then log all their values.
 // • Task 9: Use Promise, race to log the value of the first promise that resolves among multiple promises.
