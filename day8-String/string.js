4faa3b83fa978b3c95c640969289b580d6922423
Ashish Bhardwaj <erashish41@gmail.com>
2025-02-06 15:22:57 +0530

    string added

---
+++ day8-String/string.js
@@ -0,0 +1,56 @@
+// string
+let stringA = "Ashish Bhardwaj";
+let resultSa= stringA.split()
+console.log(resultSa)
+
+let resultSb = stringA.includes("ish");
+console.log(resultSb)
+
+let resultSc = stringA.startsWith("ashi");
+console.log(resultSc)
+
+let resultSd = stringA.endsWith("aj");
+console.log(resultSd)
+
+let resultSe = stringA.toUpperCase();
+console.log(resultSe)
+
+let resultSf = stringA.toLowerCase();
+console.log(resultSf)
+
+let resultSg = stringA.charAt(7);
+console.log(resultSg)
+
+let stringB = " Ashish Bhardwaj ";
+let resultSh = stringB.trim();
+console.log(resultSh)
+
+let resultSi = stringB.indexOf("a");
+console.log(resultSi)
+
+let resultSj = stringB.repeat((3));
+console.log(resultSj)
+
+let resultSk = stringB.at(10);
+console.log(resultSk)
+
+let resultSl = stringA.slice(0,-10);
+console.log(resultSl)
+
+let resultSm = stringA.slice(-10);
+console.log(resultSm)
+
+let resultSn = stringA.replace("Bhardwaj", "Ashu");
+console.log(resultSn)
+
+let resultSo = stringA.repeat(2);
+console.log(resultSo)
+
+let resultSp = stringA.split("|");
+console.log(resultSp)
+
+let resultSq = stringA.lastIndexOf("Bhardwaj")
+console.log(resultSq)
+
+let resultSr = stringA.indexOf("Bhardwaj")
+console.log(resultSr)
