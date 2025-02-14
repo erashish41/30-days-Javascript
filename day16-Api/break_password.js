3fb3c25157538193a3f05d0e3afbdf725b75b567
Ashish Bhardwaj <erashish41@gmail.com>
2025-02-14 16:13:12 +0530

    password uplock

--- day16-Api/break_password.js
+++ day16-Api/break_password.js
@@ -1,14 +1,49 @@
-// break the password
-// let passwordNumber = 777;
+//// break the password
 
+//// with only for loop
+
+let shouldBreak = false;
+let passwordNumber = "120";
+let yourPassword = "";
 for(let i=0; i<9; i++){
-    for(let j=0; j<i; j++){
-        for(let k=0; k<j; k++){
-            if(passwordNumber === "777"){
-                break
+    for(let j=0; j<9; j++){
+        for(let k=0; k<9; k++){
+            let attempt = `${i}${j}${k}`;
+
+            console.log(">>>>>>> current number >>>>>>>>>", attempt)
+            if(attempt === passwordNumber){
+                yourPassword = attempt;
+                shouldBreak = true;
+                console.log("your password is correct - ",yourPassword);
+                break;   
             }
         }
-        console.log("your password is -- ", passwordNumber);
-        
+        if(shouldBreak) break;
     }
+    if(shouldBreak) break;
 }
+
+// with function
+// function passwordUnlock(passwordNumber){
+//     let yourPassword = "";
+
+//     for(let i=0; i<10; i++){
+//         for(let j=0; j<10; j++){
+//             for(let k=0; k<10; k++){
+//                 let attempts = `${i}${j}${k}`;
+                
+//                 if(attempts === passwordNumber){
+//                     console.log("your password is correct - ",yourPassword);
+//                     return attempts;
+//                 }else{
+//                     console.log("your password is wrong - ",yourPassword);
+//                 }
+                
+//             }
+//         }
+//     }
+//     return null;
+// }
+// let userInput = prompt("Enter any 3-digit number")
+// let userResult = passwordUnlock(userInput);
+// console.log(userResult);
